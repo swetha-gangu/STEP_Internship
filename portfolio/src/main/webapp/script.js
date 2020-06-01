1860bd65019f24b903e1befdfc3e039138aa2938
Swetha Gangu <swetha.r.gangu@gmail.com>
2020-06-01 11:25:55 -0500

    add trailing new line, delete superfluous comment, add default quote container, link courses to actual websites and not coursicle

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -54,13 +54,10 @@ function addRandomQuote() {
 
 //https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp
 function placeInContainer(imgs) {
-    // Get the expanded image
     var expandImg = document.getElementById("expandedImg");
-    // Get the image text
     var imgText = document.getElementById("imgtext");
     // Use the same src in the expanded image as the image being clicked on from the grid
     expandImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
     imgText.innerHTML = imgs.alt;
     // Show the container element (hidden with CSS)
     expandImg.parentElement.style.display = "block";
