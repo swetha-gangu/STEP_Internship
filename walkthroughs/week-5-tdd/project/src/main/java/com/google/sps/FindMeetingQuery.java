289b1cf0e88ce8cceed2b3e75eaa645cef071f1e
Swetha Gangu <swetha.r.gangu@gmail.com>
2020-06-18 19:57:44 +0000

    fix one line conditionals

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -25,6 +25,15 @@ import java.util.Comparator;
 import com.google.sps.Event;
 
 public final class FindMeetingQuery {
+    private static final Comparator<Event> ORDER_BY_START = new Comparator<Event>() {
+        @Override
+        public int compare(Event first, Event next) {
+            int first_start = first.getWhen().start();
+            int next_start = next.getWhen().start();
+            return Long.compare(first_start, next_start);
+        }
+    };
+
     public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
         Collection<String> attendees = request.getAttendees(); 
         Collection<String> optional_attendees = request.getOptionalAttendees(); 
@@ -35,14 +44,18 @@ public final class FindMeetingQuery {
 
     private Collection<TimeRange> getSlotsWithOptional(Collection<String> attendees, Collection<String> optional_attendees, 
     long duration, Collection<Event> events) {
-        if (attendees.isEmpty()) return getSlots(optional_attendees, duration, events); 
+        if (attendees.isEmpty()) {
+            return getSlots(optional_attendees, duration, events); 
+        }
 
         Collection<String> combined_attendees = new HashSet<>();
         combined_attendees.addAll(attendees); 
         combined_attendees.addAll(optional_attendees); 
         Collection<TimeRange> combined_slots = getSlots(combined_attendees, duration, events); 
 
-        if (!combined_slots.isEmpty()) return combined_slots; 
+        if (!combined_slots.isEmpty()) {
+            return combined_slots; 
+        }
         else return getSlots(attendees, duration, events);
     }
 
@@ -67,11 +80,15 @@ public final class FindMeetingQuery {
 
     private Event addEvent(Event curr_event, Event last_event, Collection<TimeRange> slots, Collection<String> 
     attendees, long duration) {
-        if (!eventHasAttendee(attendees, curr_event)) return last_event; 
+        if (!eventHasAttendee(attendees, curr_event)) {
+            return last_event; 
+        }
         TimeRange curr_range = curr_event.getWhen(); 
         TimeRange last_range = last_event.getWhen(); 
-        if (curr_range.overlaps(last_range)) return getLaterTime(curr_event, last_event); 
-        else{
+        if (curr_range.overlaps(last_range)) {
+            return getLaterTime(curr_event, last_event); 
+        }
+        else {
             int curr_start = curr_range.start(); 
             int last_end = last_range.end();
             if (duration <= (curr_start - last_end)) {
@@ -85,7 +102,9 @@ public final class FindMeetingQuery {
     private Event getLaterTime(Event curr_event, Event last_event) {
         TimeRange curr_range = curr_event.getWhen(); 
         TimeRange last_range = last_event.getWhen(); 
-        if (curr_range.end() < last_range.end()) return last_event; 
+        if (curr_range.end() < last_range.end()) {
+            return last_event; 
+        }
         else return curr_event; 
     }
 
@@ -98,13 +117,4 @@ public final class FindMeetingQuery {
         }
         return false; 
     }
-
-    private static final Comparator<Event> ORDER_BY_START = new Comparator<Event>() {
-        @Override
-        public int compare(Event first, Event next) {
-            int first_start = first.getWhen().start();
-            int next_start = next.getWhen().start();
-            return Long.compare(first_start, next_start);
-        }
-    };
 }
